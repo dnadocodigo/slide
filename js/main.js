0738850b01b6830eba63b8be56f67b1d8dac1903
Paulo Galdino <dnadevnet@gmail.com>
2023-06-01 23:14:32 -0300

    add events click, mousemove and mouseup on slide

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-import * as debouce from './modules/debounces/debounce.js'
+import * as debouce from "./modules/debounces/debounce.js";
+import Slide from "./modules/slides/slide.js";
+
+const slide = new Slide(".slide", ".slide-wrapper");
+slide.init();
+
