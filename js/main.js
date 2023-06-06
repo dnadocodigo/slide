69103610af71ceab2d60dac7f0786d523cdabee6
Paulo Galdino <dnadevnet@gmail.com>
2023-06-06 03:48:13 -0300

    add pagination slide

--- js/main.js
+++ js/main.js
@@ -4,5 +4,6 @@ import { SlideNav } from "./modules/slides/slide.js";
 const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 slide.addArrow(".prev", ".next");
+slide.addControl();
 
 console.log(slide);
